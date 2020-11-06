4d16e436fdfb8dc6d444ab32b7c8f0187f3248a8
Sam B <bazinet.samuel1@gmail.com>
2020-11-06 00:17:48 -0500

    Made a skeleton of the program

--- Chess/pieces.h
+++ Chess/pieces.h
@@ -3,54 +3,59 @@
 #ifndef __pieces_h__
 #define __pieces_h__
 
-#include<string>
 #include"piece.h"
 
-class Pawn: private Piece {
+class Pawn: private virtual Piece {
 
     public:
 
         Pawn(std::string n, int x, int y, bool iW);
+        Position move();
 
 };
 
-class Rook: private Piece {
+class Rook: private virtual Piece {
 
     public:
 
         Rook(std::string n, int x, int y, bool iW);
+        Position move();
 
 };
 
-class Knight: private Piece {
+class Knight: private virtual Piece {
 
     public:
 
         Knight(std::string n, int x, int y, bool iW);
+        Position move();
 
 };
 
-class Bishop: private Piece {
+class Bishop: private virtual Piece {
 
     public:
 
         Bishop(std::string n, int x, int y, bool iW);
+        Position move();
 
 };
 
-class Queen: private Piece {
+class Queen: private virtual Piece {
 
     public:
 
         Queen(std::string n, int x, int y, bool iW);
+        Position move();
 
 };
 
-class King: private Piece {
+class King: private virtual Piece {
 
     public:
 
         King(std::string n, int x, int y, bool iW);
+        Position move();
 
 };
 
