d065647721584a878388643aa01c3cfc403d35f6
Sam B <bazinet.samuel1@gmail.com>
2021-01-06 17:49:49 -0500

    Added move functions to all the pieces

--- Chess/piece.cpp
+++ Chess/piece.cpp
@@ -23,36 +23,50 @@ Position * Pawn::move() {
     Position posi;
     // white is at the bottom of the board
     if (this->is_white) {
+
         if (firstMove) {
             posi.posX = this->pos.posX;
             posi.posY = this->pos.posY - 2;
             moves.add(posi);
         }
+
         posi.posX = this->pos.posX;
         posi.posY = this->pos.posY - 1;
-        moves.add(posi);
+        if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+            moves.add(posi);
+
         posi.posX = this->pos.posX + 1;
         posi.posY = this->pos.posY - 1;
-        moves.add(posi);
+        if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+            moves.add(posi);
+
         posi.posX = this->pos.posX - 1;
         posi.posY = this->pos.posY - 1;
-        moves.add(posi);
+        if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+            moves.add(posi);
 
     } else {
+
         if (firstMove) {
             posi.posX = this->pos.posX;
             posi.posY = this->pos.posY + 2;
             moves.add(posi);
         }
+
         posi.posX = this->pos.posX;
         posi.posY = this->pos.posY + 1;
-        moves.add(posi);
+        if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+            moves.add(posi);
+
         posi.posX = this->pos.posX + 1;
         posi.posY = this->pos.posY + 1;
-        moves.add(posi);
-        posi.posX = this->pos.posX + 1;
+        if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+            moves.add(posi);
+
+        posi.posX = this->pos.posX - 1;
         posi.posY = this->pos.posY + 1;
-        moves.add(posi);
+        if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+            moves.add(posi);
     }
 
     return moves.toArray();
@@ -65,6 +79,31 @@ Rook::Rook(int x, int y, bool iW) : Piece("rook", x, y, iW) {
 
 Position * Rook::move() {
 
+    // moves in straight lines
+
+    List<Position> moves;
+    Position posi;
+
+    for (int i = 0; i < 8; i++) {
+
+        if (i != this->pos.posX) {
+            posi.posX = i;
+            posi.posY = this->pos.posY;
+            moves.add(posi);
+        }
+    }
+
+    for (int i = 0; i < 8; i++) {
+
+        if (i != this->pos.posY) {
+            posi.posY = i;
+            posi.posX = this->pos.posX;
+            moves.add(posi);
+        }
+    }
+
+    return moves.toArray();
+
 }
 
 Knight::Knight(int x, int y, bool iW) : Piece("knight", x, y, iW) {
@@ -72,6 +111,53 @@ Knight::Knight(int x, int y, bool iW) : Piece("knight", x, y, iW) {
 }
 
 Position * Knight::move() {
+
+    // moves in L shape
+
+    List<Position> moves;
+    Position posi;
+
+    posi.posX = this->pos.posX + 1;
+    posi.posY = this->pos.posX + 2;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX = this->pos.posX - 1;
+    posi.posY = this->pos.posX + 2;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX = this->pos.posX + 2;
+    posi.posY = this->pos.posX + 1;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX = this->pos.posX - 2;
+    posi.posY = this->pos.posX + 1;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX = this->pos.posX + 2;
+    posi.posY = this->pos.posX - 1;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX = this->pos.posX - 2;
+    posi.posY = this->pos.posX - 1;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX = this->pos.posX - 1;
+    posi.posY = this->pos.posX - 2;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX = this->pos.posX + 1;
+    posi.posY = this->pos.posX - 2;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    return moves.toArray();
 
 }
 
@@ -81,6 +167,46 @@ Bishop::Bishop(int x, int y, bool iW) : Piece("bishop", x, y, iW) {
 
 Position * Bishop::move() {
 
+    //moves in diagonals
+
+    List<Position> moves;
+    Position posi;
+
+    for (int i = 0; i < 8; i++) {
+
+        if (this->pos.posX + i < 8) {
+
+            if (this->pos.posY + i < 8) {
+                posi.posX = this->pos.posX + i;
+                posi.posY = this->pos.posY + i;
+                moves.add(posi);
+            }
+
+            if (this->pos.posY - i >= 0) {
+                posi.posX = this->pos.posX + i;
+                posi.posY = this->pos.posY - i;
+                moves.add(posi);
+            }
+        }
+
+        if (this->pos.posX - i >= 0) {
+
+            if (this->pos.posY + i < 8) {
+                posi.posX = this->pos.posX - i;
+                posi.posY = this->pos.posY + i;
+                moves.add(posi);
+            }
+
+            if (this->pos.posY - i >= 0) {
+                posi.posX = this->pos.posX - i;
+                posi.posY = this->pos.posY - i;
+                moves.add(posi);
+            }
+        }
+    }
+
+    return moves.toArray();
+
 }
 
 Queen::Queen(int x, int y, bool iW) : Piece("queen", x, y, iW) {
@@ -89,12 +215,103 @@ Queen::Queen(int x, int y, bool iW) : Piece("queen", x, y, iW) {
 
 Position * Queen::move() {
 
+    // moves in diagonals and straight lines
+
+    List<Position> moves;
+    Position posi;
+
+    for (int i = 0; i < 8; i++) {
+        if (this->pos.posX + i < 8) {
+            if (this->pos.posY + i < 8) {
+                posi.posX = this->pos.posX + i;
+                posi.posY = this->pos.posY + i;
+                moves.add(posi);
+            }
+            if (this->pos.posY - i >= 0) {
+                posi.posX = this->pos.posX + i;
+                posi.posY = this->pos.posY - i;
+                moves.add(posi);
+            }
+        }
+        if (this->pos.posX - i >= 0) {
+            if (this->pos.posY + i < 8) {
+                posi.posX = this->pos.posX - i;
+                posi.posY = this->pos.posY + i;
+                moves.add(posi);
+            }
+            if (this->pos.posY - i >= 0) {
+                posi.posX = this->pos.posX - i;
+                posi.posY = this->pos.posY - i;
+                moves.add(posi);
+            }
+        }
+    }
+
+    for (int i = 0; i < 8; i++) {
+        if (i != this->pos.posX) {
+            posi.posX = i;
+            posi.posY = this->pos.posY;
+            moves.add(posi);
+        }
+    }
+
+    for (int i = 0; i < 8; i++) {
+        if (i != this->pos.posY) {
+            posi.posY = i;
+            posi.posX = this->pos.posX;
+            moves.add(posi);
+        }
+    }
+     
+
+    return moves.toArray();
+
 }
+
 
 King::King(int x, int y, bool iW) : Piece("queen", x, y, iW) {
 
 }
 
 Position * King::move() {
-    
+
+    List<Position> moves;
+    Position posi;
+
+    // top row movement
+    posi.posY = this->pos.posY + 1;
+    posi.posX = this->pos.posX - 1;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX++;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX++;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    //same row movement
+    posi.posY--;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+    posi.posX -= 2;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    // bottom row movement
+    posi.posY--;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX++;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    posi.posX++;
+    if (posi.posY < 8 && posi.posY >= 0 && posi.posX < 8 && posi.posX >= 0)
+        moves.add(posi);
+
+    return moves.toArray();
 }
